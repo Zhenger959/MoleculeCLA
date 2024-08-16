8c5c6ddbfd67d466ba683299769303bfbbe7a7a7
Zhenger959 <604799676@qq.com>
2024-08-16 16:37:01 +0800

    update load_dataset

--- docs/tutorials/load_dataset.py
+++ docs/tutorials/load_dataset.py
@@ -1,24 +1,39 @@
+import json
+
 import numpy as np
+import pandas as pd
+
+from huggingface_hub import hf_hub_download
 from datasets import load_dataset
-from src.data.components.dataset import get_label_data
+
+import pickle
 
 def main():
     targets = ['ADRB2', 'ABL1', 'CYT2C9', 'PPARG', 'GluA2', '3CL', 'HIVINT', 'HDAC2', 'KRAS', 'PDE5']
     task_names = ['docking_score', 'glide_lipo', 'glide_hbond', 'glide_evdw', 'glide_ecoul', 'glide_erotb', 'glide_esite', 'glide_emodel', 'glide_einternal']
-    target = targets[0]
+    split_dict = {'train':0, 'valid':1, 'test':2}
     
-    data = load_dataset("shikun001/MoleculeCLA", "labels")['train']
-    splits = load_dataset("shikun001/MoleculeCLA", "split")['train']
-    mol_data = load_dataset("shikun001/MoleculeCLA", "mol_data")['train']
-    idx_map = load_dataset("shikun001/MoleculeCLA", "idx_map")['train']
+    type = 'train'
+    target = targets[5]
+    file_path = hf_hub_download(repo_id="shikun001/MoleculeCLA", filename=f'labels/{target}.csv', repo_type="dataset")
+    split_file = hf_hub_download(repo_id="shikun001/MoleculeCLA", filename=f'data.csv', repo_type="dataset")
+    mol_data_file_path = hf_hub_download(repo_id="shikun001/MoleculeCLA", filename="diversity_molecule_set.pkl", repo_type="dataset")
+    idx_map_file_path = hf_hub_download(repo_id="shikun001/MoleculeCLA", filename="docking_id_idx_map.json", repo_type="dataset")
     
-    # Get all property labels
-    train_labels = get_label_data(data, splits, target, 'train', task_names)
-    valid_labels = get_label_data(data, splits, target, 'valid', task_names)
-    test_labels = get_label_data(data, splits, target, 'test', task_names)
+    # Get all training property labels
+    df = pd.read_csv(file_path)
+    splits = pd.read_csv(split_file)
+    df = df[splits['scaffold_folds'] == split_dict[type]]
+    labels = df[task_names].values.tolist()
     
     # Get all molecular information
-    id_list = splits['IDs']
+    with open(mol_data_file_path, 'rb') as f:
+        mol_data = pickle.load(f)
+    
+    with open(idx_map_file_path, 'r') as f:
+        idx_map = json.load(f)
+        
+    id_list = df['IDs'].values.tolist()
     all_index = [idx_map[x] for x in id_list]
     mol_data = np.array(mol_data)[all_index].tolist()
 
